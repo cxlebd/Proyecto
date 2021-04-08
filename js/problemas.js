b0c17fc733e7bfb6a087a24df04bdfef0f935413
cxlebd <dsanchezg1903@alumno.ipn.mx>
2021-04-07 19:42:18 -0500

    problema 1

--- js/problemas.js
+++ js/problemas.js
@@ -1,58 +1,43 @@
-function validar1(formulario){
-    /*
-    Validar que solo se introduscan numeros
-    */
-    var checkOK = "1234567890";
-    var checkStr = problema1.capital.value;
-    var allvalid = true;
-    for(var i = 0; i < checkStr.length; i++){
-        var ch = checkStr.charAt(i);
-        for(var j = 0; j < checkOK.length; j++)
-        if(ch == checkOK.charAt(j))
-            break;
-        if(j == checkOK.length){
-            allvalid = false;
-            break;
-        }
-    }
-    if(!allvalid){
-        alert("Escribe números.")
-        problema.capital.focus();
-        return false;
-    }
-    /*
-    Algoritmo
-    */
-   capital = checkStr *1.2;
-   alert("Tu capital será: " + capital);
+/*
+vamos a crear una funcion que se encargue de validar
+la entrada de los numeros apartir de la identificacion
+del codigo del evento de la tecla
+*/
+function validarn(e){
+    
+    var teclado = (document.all)?e.keyCode:e.which;
+
+    if(teclado == 8) return true;
+
+    var patron = /[0-9\d .]/;
+
+    var prueba = String.fromCharCode(teclado);
+
+    return patron.test(prueba);
 }
-function validar2(formulario){
-    /*
-    Validar que solo se introduscan numeros
-    */
-    var checkOK = "1234567890";
-    var checkStr = problema1.capital.value;
-    var allvalid = true;
-    for(var i = 0; i < checkStr.length; i++){
-        var ch = checkStr.charAt(i);
-        for(var j = 0; j < checkOK.length; j++)
-        if(ch == checkOK.charAt(j))
-            break;
-        if(j == checkOK.length){
-            allvalid = false;
-            break;
-        }
-    }
-    if(!allvalid){
-        alert("Escribe números.")
-        problema.capital.focus();
-        return false;
-    }
-    /*
-    Algoritmo
-    */
-   sueldoBase = parseInt(document.getElementById('problema2').value);   
-   comisiones = sueldoBase * .30;
-   sueldoFinal = comisiones + sueldoBase;
-   alert("Obtendrá: $" + sueldoFinal)   
+
+/*
+    Problema 1
+*/
+
+function interes(){
+    var valor = document.formulario.cantidad.value;
+    var meses = document.formulario.meses.value;
+    var resultado = parseInt(valor);
+    var resultadoMeses = parseInt(meses);
+
+    var interes = resultado*0.02*resultadoMeses;
+    var total = resultado + interes;
+
+    document.formulario.sueldoti.value="$"+total;
 }
+
+function borrar(){
+    document.formulario.cantidad.value="";
+    document.formulario.sueldoti.value="";
+}
+
+/*
+    Problema 2
+*/
+
